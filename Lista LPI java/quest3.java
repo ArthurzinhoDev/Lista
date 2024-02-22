a7072a34d038cb82aa0e40293e1d5eaf133762e5
ArthurzinhoDev <152618050+ArthurzinhoDev@users.noreply.github.com>
2024-02-22 16:24:09 -0300

    Add files via upload

---
+++ Lista LPI java/quest3.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+/*3-     Dados seis números inteiros representando dois intervalos de tempo (horas, minutos e
+        segundos), faça um programa para calcular a soma e a diferença desses intervalos.*/
+
+public class quest3{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.print("\nDigite os seis números inteiros representando dois intervalos de tempo (horas, minutos e segundos).\nHoras:");
+            int hora1 = scan.nextInt();
+        System.out.print("Minutos:");
+            int min1 = scan.nextInt();
+        System.out.print("Segundos:");
+            int s1 = scan.nextInt();
+
+
+        System.out.print("\nSegundo intervalo de tempo (horas, minutos e segundos).\nHoras:");
+            int hora2 = scan.nextInt();
+        System.out.print("Minutos:");
+            int min2 = scan.nextInt();
+        System.out.print("Segundos:");
+            int s2 = scan.nextInt();
+
+            int somaH = hora1 + hora2;
+            int somaM = min1 + min2;
+            int somaS = s1 + s2;
+        if(somaS>=60){
+            somaS -=60;
+            somaM++;
+        }
+        if(somaM>=60){
+            somaM -=60;
+            somaH++;
+        }
+            int difH = hora1 - hora2;
+            int difM = min1 - min2;
+            int difS = s1 - s2;
+
+        if(difS<0){
+            difS+=60;
+            difM--;
+        }
+        if(difM<0){
+            difM +=60;
+            difH--;
+        }
+        System.out.print("A soma dos dois intervalos de tempo é "+somaH+"h:"+somaM+"min:"+somaS+"seg.");
+        System.out.print("\nA diferenca dos dois intervalos de tempo é "+difH+"h:"+difM+"min:"+difS+"seg.");
+    }
+}
